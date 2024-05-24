b1c46ecd97f24d09f3ab947b79c9f34a068d7761
Ibryam512 <bamko2003@gmail.com>
2024-05-24 10:59:38 +0300

    Add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { CharactersListComponent } from './features/characters/components/characters-list/characters-list.component';
 import { EpisodesListComponent } from './features/episodes/components/episodes-list/episodes-list.component';
 import { LocationsListComponent } from './features/locations/components/locations-list/locations-list.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'characters',
     component: CharactersListComponent,
---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatDivider],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
